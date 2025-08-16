6b9fd3a358c74cc0b1703bc03c56190cac564d1b
shashwat030900 <3dartist.shashwat@gmail.com>
2025-08-16 15:57:38 +0530

    Level_data

--- Linked-List-Snake/include/Level/LevelData.h
+++ Linked-List-Snake/include/Level/LevelData.h
@@ -1,8 +1,16 @@
 #pragma once
+#include "Level/LevelService.h"
+#include "Level/LevelNumber.h"
 
 namespace Level
 {
     struct LevelData
     {
+        LevelData(LevelNumber ind)
+        {
+            level_index = ind;
+        }
+
+        LevelNumber level_index;
     };
 }
