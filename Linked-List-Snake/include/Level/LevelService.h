fd6c0136c77f27e23f72180e8609b04d60ce327c
shashwat030900 <3dartist.shashwat@gmail.com>
2025-08-16 16:18:48 +0530

    .

--- Linked-List-Snake/include/Level/LevelService.h
+++ Linked-List-Snake/include/Level/LevelService.h
@@ -4,11 +4,16 @@
 
 namespace Level
 {
+    class LevelController;
+
     class LevelService
     {
     private:
         LevelController* level_controller;
         LevelNumber current_level;
+
+        void createLevelController();
+        void destroy();
 
     public:
         LevelService();
--- Linked-List-Snake/source/Level/LevelService.cpp
+++ Linked-List-Snake/source/Level/LevelService.cpp
@@ -1,35 +1,50 @@
-#include "../../include/Level/LevelService.h"
-#include "../../include/Level/LevelController.h"
+#include "Level/LevelService.h"
+#include "Level/LevelController.h"
+#include "Global/ServiceLocator.h"
 
 namespace Level
 {
-    LevelService::LevelService()
-    {
-        level_controller = new LevelController();
-    }
+	using namespace Global;
 
-    LevelService::~LevelService()
-    {
-        delete level_controller;
-    }
+	LevelService::LevelService()
+	{
+		level_controller = nullptr;
 
-    void LevelService::initialize()
-    {
-        level_controller->initialize();
-    }
+		createLevelController();
+	}
 
-    void LevelService::update()
-    {
-        level_controller->update();
-    }
+	LevelService::~LevelService()
+	{
+		destroy();
+	}
 
-    void LevelService::render()
-    {
-        level_controller->render();
-    }
+	void LevelService::createLevelController()
+	{
+		level_controller = new LevelController();
+	}
 
-    void LevelService::createLevel(LevelNumber level_to_load)
-    {
-        current_level = level_to_load;
-    }
+	void LevelService::initialize()
+	{
+		level_controller->initialize();
+	}
+
+	void LevelService::update()
+	{
+		level_controller->update();
+	}
+
+	void LevelService::render()
+	{
+		level_controller->render();
+	}
+
+	void LevelService::createLevel(LevelNumber level_to_load)
+	{
+		current_level = level_to_load;
+	}
+
+	void LevelService::destroy()
+	{
+		delete level_controller;
+	}
 }
