7a05d72064864f0b50a03427fb965b44a8f540d9
shashwat030900 <3dartist.shashwat@gmail.com>
2025-08-19 12:17:06 +0530

    Draw BodyPart

--- Linked-List-Snake/include/Player/BodyPart.h
+++ Linked-List-Snake/include/Player/BodyPart.h
@@ -1,20 +1,46 @@
-#pragma once
+﻿#pragma once
 
 #include <SFML/System/Vector2.hpp>
 #include "Direction.h"
-#include "UI/UIElement/ImageView.h" 
+#include "UI/UIElement/ImageView.h"
+#include "Global/Config.h"  
 
 namespace Player
 {
-	class BodyPart
-	{
-	protected:
-		UI::UIElement::ImageView* bodypart_image;
+    class BodyPart
+    {
+    protected:
+        UI::UIElement::ImageView* bodypart_image;
 
-		sf::Vector2i grid_position;
-		Direction direction;
+        sf::Vector2i grid_position;
+        Direction    direction;
 
-		float bodypart_width;
-		float bodypart_height;
-	};
+        float bodypart_width{};
+        float bodypart_height{};
+
+        void createBodyPartImage();
+        void destroy();
+
+        
+        void initializeBodyPartImage();
+        sf::Vector2f getBodyPartScreenPosition() const;
+        void syncImageToGrid();
+
+    public:
+        BodyPart();
+        virtual ~BodyPart();
+
+        
+        void initialize(float width, float height, sf::Vector2i pos, Direction dir);
+        void update();
+        void render();
+
+        
+        void reset();
+        void handleRestart();
+        void processPlayerInput();
+        void updateSnakeDirection();
+        void moveSnake();
+        void processSnakeCollision();
+    };
 }
---
+++ Linked-List-Snake/source/Player/BodyPart.cpp
@@ -0,0 +1,85 @@
+﻿#include "Player/BodyPart.h"
+#include "Global/Config.h"
+
+namespace Player
+{
+    BodyPart::BodyPart()
+        : grid_position(0, 0), bodypart_image(nullptr)
+    {
+        createBodyPartImage();
+    }
+
+    BodyPart::~BodyPart()
+    {
+        destroy();
+    }
+
+    void BodyPart::createBodyPartImage()
+    {
+        if (!bodypart_image)
+            bodypart_image = new UI::UIElement::ImageView();
+    }
+
+   
+    void BodyPart::initializeBodyPartImage()
+    {
+        if (!bodypart_image) createBodyPartImage();
+
+        bodypart_image->initialize(
+			Global::Config::snake_body_texture_path,
+            bodypart_width,
+            bodypart_height,
+            getBodyPartScreenPosition()
+        );
+        bodypart_image->setOriginAtCentre();
+    }
+
+    
+    sf::Vector2f BodyPart::getBodyPartScreenPosition() const
+    {
+        
+        return sf::Vector2f(
+            grid_position.x * bodypart_width + bodypart_width * 0.5f,
+            grid_position.y * bodypart_height + bodypart_height * 0.5f
+        );
+    }
+
+   
+    void BodyPart::syncImageToGrid()
+    {
+        if (bodypart_image)
+            bodypart_image->setPosition(getBodyPartScreenPosition());
+    }
+
+  
+    void BodyPart::initialize(float width, float height, sf::Vector2i pos, Direction dir)
+    {
+        bodypart_width = width;
+        bodypart_height = height;
+        direction = dir;
+        grid_position = pos;
+
+        initializeBodyPartImage();
+    }
+
+    void BodyPart::render()
+    {
+        if (bodypart_image)
+            bodypart_image->render();
+    }
+
+    void BodyPart::destroy()
+    {
+        delete bodypart_image;
+        bodypart_image = nullptr;
+    }
+
+    
+    void BodyPart::reset() {}
+    void BodyPart::handleRestart() {}
+    void BodyPart::processPlayerInput() {}
+    void BodyPart::updateSnakeDirection() {}
+    void BodyPart::moveSnake() { }
+    void BodyPart::processSnakeCollision() {}
+    void BodyPart::update() { }
+}
