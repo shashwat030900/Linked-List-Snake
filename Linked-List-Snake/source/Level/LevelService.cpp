06db0f221fdcbcf2480de9ddee411a05908136b4
shashwat030900 <3dartist.shashwat@gmail.com>
2025-09-27 11:15:59 +0530

    Create Level 2

--- Linked-List-Snake/source/Level/LevelService.cpp
+++ Linked-List-Snake/source/Level/LevelService.cpp
@@ -40,7 +40,7 @@ namespace Level
     void LevelService::createLevel(LevelNumber level_to_load)
     {
         current_level = level_to_load;
-        level_controller->loadLevel(level_to_load);
+        //level_controller->loadLevel(level_to_load);
         spawnPlayer();
 		spawnLevelElements(level_to_load);
     }
